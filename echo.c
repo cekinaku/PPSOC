0690d609a9e636177735d4e6abece23c06e818c6
fjakovac <63203956+cekinaku@users.noreply.github.com>
2024-01-25 06:02:35 +0100

    Add files via upload

--- echo.c
+++ echo.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 - 2019 Xilinx, Inc.
+ * Copyright (C) 2016 - 2019 Xilinx, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -29,222 +29,123 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "lwip/err.h"
-#include "lwip/tcp.h"
-#if defined (__arm__) || defined (__aarch64__)
-#include "xil_printf.h"
-#endif
+#include "lwip/sockets.h"
 #include "netif/xadapter.h"
+#include "lwipopts.h"
+#include "xil_printf.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-struct tcp_pcb * pcbGlobal;
-u32_t bytes_to_send;
-int last_packet_sent;
-void *send_head;
-struct netif *globalNetif;
-int packets_sent = 0;
+#include "axiVdmaRegisters.h"
 
-int transfer_data() {
-	return 0;
+#define THREAD_STACKSIZE 1024
+/*
+ * Max number of telnet connections that this application can serve.
+ * The existing implementation does not support closing of an existing telnet.
+ * Once a telnet connection is made, it stays for ever.
+ */
+#define MAX_CONNECTIONS 8
+
+typedef uint32_t pixel_t;
+
+int new_sd[MAX_CONNECTIONS];
+int connection_index;
+
+u16_t echo_port = 7;
+
+vdma_handle *handle;
+
+void print_echo_app_header()
+{
+    xil_printf("%20s %6d %s\r\n", "echo server",
+                        echo_port,
+                        "$ telnet <board_ip> 7");
+
 }
 
-void print_app_header()
+/* thread spawned for each connection */
+void process_echo_request(void *p)
 {
-#if (LWIP_IPV6==0)
-	xil_printf("\n\r\n\r-----lwIP TCP echo server ------\n\r");
+	int sd = *(int *)p;
+	uint32_t frame_size = HEIGHT * WIDTH * sizeof(pixel_t);
+	while (1) {
+
+    	xil_printf("\nSend frame\n");
+    	int status = vdma_get(handle, OFFSET_VDMA_S2MM_STATUS_REGISTER);
+
+    	switch ((status & VDMA_STATUS_REGISTER_FrameCount) >> 16) {
+    		case 1:
+    			write(sd, handle->fb3PhysicalAddress, frame_size);
+    			break;
+    		case 2:
+    			write(sd, handle->fb1PhysicalAddress, frame_size);
+    			break;
+    		case 3:
+    			write(sd, handle->fb2PhysicalAddress, frame_size);
+    			break;
+    		default:
+    	}
+
+
+	}
+
+	/* close connection */
+	close(sd);
+	vTaskDelete(NULL);
+}
+
+void echo_application_thread()
+{
+	int sock;
+	int size;
+#if LWIP_IPV6==0
+	struct sockaddr_in address, remote;
+
+	memset(&address, 0, sizeof(address));
+
+	if ((sock = lwip_socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		return;
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(echo_port);
+	address.sin_addr.s_addr = INADDR_ANY;
 #else
-	xil_printf("\n\r\n\r-----lwIPv6 TCP echo server ------\n\r");
+	struct sockaddr_in6 address, remote;
+
+	memset(&address, 0, sizeof(address));
+
+	address.sin6_len = sizeof(address);
+	address.sin6_family = AF_INET6;
+	address.sin6_port = htons(echo_port);
+
+	memset(&(address.sin6_addr), 0, sizeof(address.sin6_addr));
+
+	if ((sock = lwip_socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+		return;
 #endif
-	xil_printf("TCP packets sent to port 6001 will be echoed back\n\r");
-}
 
-err_t recv_callback(void *arg, struct tcp_pcb *tpcb,
-                               struct pbuf *p, err_t err)
-{
-
-	/* do not read the packet if we are not in ESTABLISHED state */
-	if (!p) {
-		tcp_close(tpcb);
-		tcp_recv(tpcb, NULL);
-		return ERR_OK;
-	}
-
-	/* indicate that the packet has been received */
-	tcp_recved(tpcb, p->len);
-
-	/* echo back the payload */
-	/* in this case, we assume that the payload is < TCP_SND_BUF */
-	if (tcp_sndbuf(tpcb) > p->len) {
-		err = tcp_write(tpcb, p->payload, p->len, 1);
-		xil_printf("txperf: status on tcp_write: %d\r\n", err);
-	} else
-		xil_printf("no space in tcp_sndbuf\n\r");
-
-	/* free the received pbuf */
-	pbuf_free(p);
-
-	return ERR_OK;
-}
-
-err_t sent_callback(void *arg, struct tcp_pcb *tpcb, u16_t len) {
-
-	//xil_printf("Entered Sent callback\n");
-	//uint16_t tcp_sndf = tcp_sndbuf(tpcb);
-	//xil_printf("tcp_sndbuf cb %d\n", tcp_sndf);
-
-	uint32_t max_bytes = 10024;
-	uint32_t packet_size = bytes_to_send;
-	err_t status;
-	//xil_printf("bytes to send start %d\n", bytes_to_send);
-	//xil_printf("initial packer size %d\n", packet_size);
-
-	packets_sent++;
-	if (bytes_to_send == 0) {
-		//xil_printf("bytes to send is 0");
-		//xil_printf("Packets per frame: %d", packets_sent);
-		last_packet_sent = 1;
-		return ERR_OK;
-	}
-
-	if (packet_size > max_bytes) {
-		//xil_printf("packet size is larger than max bytes limit \n");
-		packet_size = max_bytes;
-		//xil_printf("packet_size %d\n", packet_size);
-	}
-
-	status = tcp_write(tpcb, send_head, packet_size, 0);
-	if (status != ERR_OK) {
-		xil_printf("txperf: Error on tcp_write: %d\r\n", status);
-		return status;
-	}
-
-	//xil_printf("compariosn packetSize %d\n", packet_size);
-
-	if (packet_size == bytes_to_send) {
-		bytes_to_send = 0;
-	} else {
-		bytes_to_send -= packet_size;
-	}
-
-	//xil_printf("bytes to send reduced %d\n", bytes_to_send);
-
-	send_head += packet_size;
-
-	status = tcp_output(tpcb);
-	if (status != ERR_OK) {
-		xil_printf("txperf: Error on tcp_output: %d\r\n", status);
-		return status;
-	}
-
-
-    return status;
-}
-
-err_t accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err)
-{
-	static int connection = 1;
-
-	/* set the receive callback for this connection */
-	tcp_recv(newpcb, recv_callback);
-
-	/* just use an integer number indicating the connection id as the
-	   callback argument */
-	tcp_arg(newpcb, (void*)(UINTPTR)connection);
-
-	tcp_sent(newpcb, sent_callback);
-
-	// set global pcb
-	pcbGlobal = newpcb;
-	if (newpcb->state == ESTABLISHED) {
-		xil_printf("Conn established\n");
-	}
-
-
-	/* increment for subsequent accepted connections */
-	connection++;
-
-	return ERR_OK;
-}
-
-
-int start_application(struct netif *netif)
-{
-	globalNetif = netif;
-	struct tcp_pcb *pcb;
-	err_t err;
-	unsigned port = 7;
-
-	/* create new TCP PCB structure */
-	pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
-	if (!pcb) {
-		xil_printf("Error creating PCB. Out of Memory\n\r");
-		return -1;
-	}
-
-	/* bind to specified @port */
-	err = tcp_bind(pcb, IP_ANY_TYPE, port);
-	if (err != ERR_OK) {
-		xil_printf("Unable to bind to port %d: err = %d\n\r", port, err);
-		return -2;
-	}
-
-	/* we do not need any arguments to callback functions */
-	tcp_arg(pcb, NULL);
-
-	/* listen for connections */
-	pcb = tcp_listen(pcb);
-	if (!pcb) {
-		xil_printf("Out of memory while tcp_listen\n\r");
-		return -3;
-	}
-
-	/* specify callback to use for incoming connections */
-	tcp_accept(pcb, accept_callback);
-
-	xil_printf("TCP echo server started @ port %d\n\r", port);
-
-	return 0;
-}
-
-void send_data(struct tcp_pcb *pcb, uint32_t *dataAddress, u32_t dataSize){
-
-	uint32_t packetSize;
-	bytes_to_send = dataSize;
-	err_t status;
-	uint32_t max_bytes = 10024;
-	last_packet_sent = 0;
-
-	send_head = dataAddress;
-	if (!pcb){
-		xil_printf("\n pcb not initialized  \n");
-	}
-
-	packetSize = max_bytes;
-
-	status = tcp_write(pcb, send_head, packetSize, 0);
-	if (status != ERR_OK) {
-		xil_printf("txperf: Error on tcp_write: %d\r\n", status);
+	if (lwip_bind(sock, (struct sockaddr *)&address, sizeof (address)) < 0)
 		return;
-	}
 
-	send_head += packetSize;
-	bytes_to_send -= packetSize;
+	lwip_listen(sock, 0);
 
-	status = tcp_output(pcb);
-	if (status != ERR_OK) {
-		xil_printf("txperf: Error on tcp_output: %d\r\n", status);
-		return;
-	}
-	packets_sent++;
+	size = sizeof(remote);
 
 	while (1) {
-		xemacif_input(globalNetif);
-		if (last_packet_sent == 1) {
-			xil_printf("\na ACKED\n");
-			return;
+		if ((new_sd[connection_index] = lwip_accept(sock, (struct sockaddr *)&remote, (socklen_t *)&size)) > 0) {
+			sys_thread_new("echos", process_echo_request,
+				(void*)&(new_sd[connection_index]),
+				THREAD_STACKSIZE,
+				DEFAULT_THREAD_PRIO);
+			if (++connection_index>= MAX_CONNECTIONS) {
+				break;
+			}
 		}
 	}
+	xil_printf("Maximum number of connections reached, No further connections will be accepted\r\n");
+	vTaskSuspend(NULL);
 }
 
-struct tcp_pcb * getGlobalPcb() {
-	return pcbGlobal;
+void passAxiVdmaPtr(vdma_handle *handler) {
+	handle = handler;
 }
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 - 2019 Xilinx, Inc.
+ * Copyright (C) 2016 - 2019 Xilinx, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -27,47 +27,67 @@
  */
 
 #include <stdio.h>
-
 #include "xparameters.h"
-
 #include "netif/xadapter.h"
-
-#include "platform.h"
 #include "platform_config.h"
-#if defined (__arm__) || defined(__aarch64__)
 #include "xil_printf.h"
-#endif
 
-#include "lwip/tcp.h"
-#include "xil_cache.h"
-
-#include "xiicps.h"
 #include "axiVdmaRegisters.h"
 #include "cameraRegisters.h"
 
-typedef uint32_t pixel_t;
+#if LWIP_IPV6==1
+#include "lwip/ip.h"
+#else
+#if LWIP_DHCP==1
+#include "lwip/dhcp.h"
+#endif
+#endif
 
-/* defined by each RAW mode application */
-void print_app_header();
-int start_application(struct netif *netif);
-int transfer_data();
-void tcp_fasttmr(void);
-void tcp_slowtmr(void);
-void send_data(struct tcp_pcb *pcb, uint16_t *dataAddress, u32_t dataSize);
-struct tcp_pcb * getGlobalPcb();
+#ifdef XPS_BOARD_ZCU102
+#ifdef XPAR_XIICPS_0_DEVICE_ID
+int IicPhyReset(void);
+#endif
+#endif
+
+int main_thread();
+void print_echo_app_header();
+void echo_application_thread(void *);
 void init_ov7670();
-
-
-/* missing declaration in lwIP */
 void lwip_init();
+void passAxiVdmaPtr(vdma_handle *handler);
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+extern volatile int dhcp_timoutcntr;
+err_t dhcp_start(struct netif *netif);
+#endif
+#endif
+
+#define THREAD_STACKSIZE 1024
 
 static struct netif server_netif;
 struct netif *echo_netif;
 
+#if LWIP_IPV6==1
+void print_ip6(char *msg, ip_addr_t *ip)
+{
+	print(msg);
+	xil_printf(" %x:%x:%x:%x:%x:%x:%x:%x\n\r",
+			IP6_ADDR_BLOCK1(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK2(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK3(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK4(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK5(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK6(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK7(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK8(&ip->u_addr.ip6));
+}
+
+#else
 void
 print_ip(char *msg, ip_addr_t *ip)
 {
-	print(msg);
+	xil_printf(msg);
 	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip),
 			ip4_addr3(ip), ip4_addr4(ip));
 }
@@ -81,57 +101,134 @@ print_ip_settings(ip_addr_t *ip, ip_addr_t *mask, ip_addr_t *gw)
 	print_ip("Gateway : ", gw);
 }
 
-#if defined (__arm__) && !defined (ARMR5)
-#if XPAR_GIGE_PCS_PMA_SGMII_CORE_PRESENT == 1 || XPAR_GIGE_PCS_PMA_1000BASEX_CORE_PRESENT == 1
-int ProgramSi5324(void);
-int ProgramSfpPhy(void);
 #endif
+int main()
+{
+	sys_thread_new("main_thrd", (void(*)(void*))main_thread, 0,
+	                THREAD_STACKSIZE,
+	                DEFAULT_THREAD_PRIO);
+	vTaskStartScheduler();
+	while(1);
+	return 0;
+}
+
+void network_thread(void *p)
+{
+    struct netif *netif;
+    /* the mac address of the board. this should be unique per board */
+    unsigned char mac_ethernet_address[] = { 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
+#if LWIP_IPV6==0
+    ip_addr_t ipaddr, netmask, gw;
+#if LWIP_DHCP==1
+    int mscnt = 0;
+#endif
+#endif
+
+    netif = &server_netif;
+
+    xil_printf("\r\n\r\n");
+    xil_printf("-----lwIP Socket Mode Echo server Demo Application ------\r\n");
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==0
+    /* initialize IP addresses to be used */
+    IP4_ADDR(&ipaddr,  192, 168, 1, 10);
+    IP4_ADDR(&netmask, 255, 255, 255,  0);
+    IP4_ADDR(&gw,      192, 168, 1, 1);
+#endif
+
+    /* print out IP settings of the board */
+
+#if LWIP_DHCP==0
+    print_ip_settings(&ipaddr, &netmask, &gw);
+    /* print all application headers */
+#endif
+
+#if LWIP_DHCP==1
+	ipaddr.addr = 0;
+	gw.addr = 0;
+	netmask.addr = 0;
+#endif
+#endif
+
+#if LWIP_IPV6==0
+    /* Add network interface to the netif_list, and set it as default */
+    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_ethernet_address, PLATFORM_EMAC_BASEADDR)) {
+	xil_printf("Error adding N/W interface\r\n");
+	return;
+    }
+#else
+    /* Add network interface to the netif_list, and set it as default */
+    if (!xemac_add(netif, NULL, NULL, NULL, mac_ethernet_address, PLATFORM_EMAC_BASEADDR)) {
+	xil_printf("Error adding N/W interface\r\n");
+	return;
+    }
+
+    netif->ip6_autoconfig_enabled = 1;
+
+    netif_create_ip6_linklocal_address(netif, 1);
+    netif_ip6_addr_set_state(netif, 0, IP6_ADDR_VALID);
+
+    print_ip6("\n\rBoard IPv6 address ", &netif->ip6_addr[0].u_addr.ip6);
+#endif
+
+    netif_set_default(netif);
+
+    /* specify that the network if is up */
+    netif_set_up(netif);
+
+    /* start packet receive thread - required for lwIP operation */
+    sys_thread_new("xemacif_input_thread", (void(*)(void*))xemacif_input_thread, netif,
+            THREAD_STACKSIZE,
+            DEFAULT_THREAD_PRIO);
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+    dhcp_start(netif);
+    while (1) {
+		vTaskDelay(DHCP_FINE_TIMER_MSECS / portTICK_RATE_MS);
+		dhcp_fine_tmr();
+		mscnt += DHCP_FINE_TIMER_MSECS;
+		if (mscnt >= DHCP_COARSE_TIMER_SECS*1000) {
+			dhcp_coarse_tmr();
+			mscnt = 0;
+		}
+	}
+#else
+    xil_printf("\r\n");
+    xil_printf("%20s %6s %s\r\n", "Server", "Port", "Connect With..");
+    xil_printf("%20s %6s %s\r\n", "--------------------", "------", "--------------------");
+
+    print_echo_app_header();
+    xil_printf("\r\n");
+    sys_thread_new("echod", echo_application_thread, 0,
+		THREAD_STACKSIZE,
+		DEFAULT_THREAD_PRIO);
+    vTaskDelete(NULL);
+#endif
+#else
+    print_echo_app_header();
+    xil_printf("\r\n");
+    sys_thread_new("echod",echo_application_thread, 0,
+		THREAD_STACKSIZE,
+		DEFAULT_THREAD_PRIO);
+    vTaskDelete(NULL);
+#endif
+    return;
+}
+
+int main_thread()
+{
+#if LWIP_DHCP==1
+	int mscnt = 0;
 #endif
 
 #ifdef XPS_BOARD_ZCU102
-#ifdef XPAR_XIICPS_0_DEVICE_ID
-int IicPhyReset(void);
-#endif
+	IicPhyReset();
 #endif
 
-int main()
-{
-	ip_addr_t ipaddr, netmask, gw;
-
-	/* the mac address of the board. this should be unique per board */
-	unsigned char mac_ethernet_address[] =
-	{ 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
-
-	echo_netif = &server_netif;
-	init_platform();
-
-	/* initialize IP addresses to be used */
-	IP4_ADDR(&ipaddr,  192, 168,   1, 10);
-	IP4_ADDR(&netmask, 255, 255, 255,  0);
-	IP4_ADDR(&gw,      192, 168,   1,  1);
-	print_app_header();
-
-	lwip_init();
-
-	/* Add network interface to the netif_list, and set it as default */
-	if (!xemac_add(echo_netif, &ipaddr, &netmask,
-						&gw, mac_ethernet_address,
-						PLATFORM_EMAC_BASEADDR)) {
-		xil_printf("Error adding N/W interface\n\r");
-		return -1;
-	}
-	netif_set_default(echo_netif);
-
-	/* now enable interrupts */
-	platform_enable_interrupts();
-
-	/* specify that the network if is up */
-	netif_set_up(echo_netif);
-
-	print_ip_settings(&ipaddr, &netmask, &gw);
-
-    xil_printf("Start Initialize Camera Module, reset before usage\n");
-    init_ov7670();
+	xil_printf("Start Initialize Camera Module, reset before usage\n");
+	init_ov7670();
 
     xil_printf("\nSetUp VMDA\n");
     vdma_handle handle_s2mm;
@@ -146,45 +243,53 @@ int main()
     vdma_set(&handle_s2mm, OFFSET_VDMA_S2MM_STATUS_REGISTER, 0);
     vdma_s2mm_status_dump(&handle_s2mm);
 
-	/* start the application (web server, rxtest, txtest, etc..) */
+    passAxiVdmaPtr(&handle_s2mm);
 
+	/* initialize lwIP before calling sys_thread_new */
+    lwip_init();
 
-    struct tcp_pcb *pcb;
-    start_application(echo_netif);
-    int idx = 0;
+    /* any thread using lwIP should be created using sys_thread_new */
+    sys_thread_new("NW_THRD", network_thread, NULL,
+		THREAD_STACKSIZE,
+            DEFAULT_THREAD_PRIO);
 
-	/* receive and process packets */
-    int connected = 0;
-	while (connected != 1) {
-		xemacif_input(echo_netif);
-		pcb = getGlobalPcb();
-		if (pcb->state == ESTABLISHED) {
-				xil_printf("Conn established\n");
-				connected = 1;
-			}
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+    while (1) {
+	vTaskDelay(DHCP_FINE_TIMER_MSECS / portTICK_RATE_MS);
+		if (server_netif.ip_addr.addr) {
+			xil_printf("DHCP request success\r\n");
+			print_ip_settings(&(server_netif.ip_addr), &(server_netif.netmask), &(server_netif.gw));
+			print_echo_app_header();
+			xil_printf("\r\n");
+			sys_thread_new("echod", echo_application_thread, 0,
+					THREAD_STACKSIZE,
+					DEFAULT_THREAD_PRIO);
+			break;
+		}
+		mscnt += DHCP_FINE_TIMER_MSECS;
+		if (mscnt >= DHCP_COARSE_TIMER_SECS * 2000) {
+			xil_printf("ERROR: DHCP request timed out\r\n");
+			xil_printf("Configuring default IP of 192.168.1.10\r\n");
+			IP4_ADDR(&(server_netif.ip_addr),  192, 168, 1, 10);
+			IP4_ADDR(&(server_netif.netmask), 255, 255, 255,  0);
+			IP4_ADDR(&(server_netif.gw),  192, 168, 1, 1);
+			print_ip_settings(&(server_netif.ip_addr), &(server_netif.netmask), &(server_netif.gw));
+			/* print all application headers */
+			xil_printf("\r\n");
+			xil_printf("%20s %6s %s\r\n", "Server", "Port", "Connect With..");
+			xil_printf("%20s %6s %s\r\n", "--------------------", "------", "--------------------");
+
+			print_echo_app_header();
+			xil_printf("\r\n");
+			sys_thread_new("echod", echo_application_thread, 0,
+					THREAD_STACKSIZE,
+					DEFAULT_THREAD_PRIO);
+			break;
+		}
 	}
-	uint32_t frame_size = HEIGHT * WIDTH * sizeof(pixel_t);
-
-    while(1) {
-    	xil_printf("\nSend %d frame\n", idx++);
-    	int status = vdma_get(&handle_s2mm, OFFSET_VDMA_S2MM_STATUS_REGISTER);
-
-    	switch ((status & VDMA_STATUS_REGISTER_FrameCount) >> 16) {
-    		case 1:
-    			send_data(pcb, handle_s2mm.fb3PhysicalAddress, frame_size);
-    			break;
-    		case 2:
-    			send_data(pcb, handle_s2mm.fb1PhysicalAddress, frame_size);
-    			break;
-    		case 3:
-    			send_data(pcb, handle_s2mm.fb2PhysicalAddress, frame_size);
-    			break;
-    		default:
-    	}
-    }
-
-	/* never reached */
-	cleanup_platform();
-
-	return 0;
+#endif
+#endif
+    vTaskDelete(NULL);
+    return 0;
 }
